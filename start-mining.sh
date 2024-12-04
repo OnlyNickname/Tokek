e15709009ee0a55e22ea7cf706e9c6f3f7577704
Duwik <norkholifan1@gmail.com>
2024-12-04 22:14:20 +0700

    first commit

---
+++ start-mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Ubah path sesuai dengan struktur di dalam kontainer
+cd /SRBMiner-Multi-2-7-1
+
+# Perintah untuk memulai mining
+./SRBMiner-MULTI --algorithm verushash --pool ap.luckpool.net:3956 --wallet RAKB3nx5mynwwcU27rY18VTi2q9Zhz1Eeo --Worker Tokek --password x --cpu-threads 0
